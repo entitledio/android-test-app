9e5b79bf5937a708d4104839a59f27e1aa23a4b9
Wontae Yang <wontaeyang@gmail.com>
2018-08-03 09:13:46 -0400

    replace String.join with TextUtils for compatibility

--- app/src/main/java/com/entitled/example/test/MainActivity.java
+++ app/src/main/java/com/entitled/example/test/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -62,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
                 responses.add(json);
             }
 
-            String allPurchases = String.join(", \n", responses);
+            String allPurchases = TextUtils.join(", \n", responses);
             updateText(allPurchases);
         } else {
             updateText("No purchases or subscriptions found.");
